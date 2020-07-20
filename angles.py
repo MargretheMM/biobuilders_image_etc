4b27ea31a9e2bdf176069e2cb21709c5c33b6580
MargretheMM <63294890+MargretheMM@users.noreply.github.com>
2020-07-20 11:22:49 +0200

    avoid division by zero

--- angles.py
+++ angles.py
@@ -2,6 +2,7 @@ import networkx as nx
 import numpy as np
 import scipy.spatial
 import sys
+import ntpath
 import math
 
 G = nx.read_gpickle(sys.argv[1])
@@ -80,7 +81,10 @@ def angle(vertex,side1, side2):
     l = coords(side1, v)
     r = coords(side2, v)
     # calculating the cos of the angle between the l and r vectors
-    res = (l[0]*r[0]+l[1]*r[1]) / ((l[0]**2+l[1]**2)**.5 * (r[0]**2+r[1]**2)**.5)
+    denom = (l[0]**2+l[1]**2)**.5 * (r[0]**2+r[1]**2)**.5    
+    if (denom == 0):
+        return 'no angle' 
+    res = (l[0]*r[0]+l[1]*r[1]) / denom
     # catch floating point inaccuracy
     if res < -1:
         res = -1
@@ -91,8 +95,11 @@ def angle(vertex,side1, side2):
     return rad
 
 
+def prettify(angles):
+    return "\n".join(["  %s: %s" % (k, v) for k, v in angles.iteritems()])
+
 f = open(sys.argv[1] + '.txt', 'w')
-f.write('*** Statistics for ' + graph_name + ' ***\n\n')
+f.write('*** Statistics ***\n\n')
 f.write('Number of junctions:\t %d\n'%NumberOfJunctions(G))
 f.write('Number of tips:\t\t %d\n'%NumberOfTips(G))
 f.write('Total length:\t\t %f px\n'%TotalLength(G))
@@ -101,9 +108,8 @@ f.write('Average edge radius:\t %f px\n'%AverageEdgeRadius(G))
 f.write('Total network area:\t %f px^2\n'%TotalNetworkArea(G))
 f.write('Area of convex hull:\t %f px^2\n'%AreaOfConvexHull(G))
 f.write('Number of cycles:\t %d\n'%NumberOfCycles(G))
-f.write('Angles - nodes with 2 neightbors:\n'Anglecalc(G,2))
-f.write('Angles - nodes with 3 neightbors:\n'Anglecalc(G,3))
-f.write('Angles - nodes with 4 neightbors:\n'Anglecalc(G,4)) 
+for i in range(2, 5):
+    f.write('Angles - nodes with %d neightbors:\n %s\n' % (i, prettify(Anglecalc(G,i))))
 f.close()
 
 
